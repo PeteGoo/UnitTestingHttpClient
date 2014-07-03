2d9a9715270d5c78c9c264d931650b509bffd0f0
petegoo <pete@petegoo.com>
2014-07-03 15:49:52 +1200

    Initial Code Commit

---
+++ UnitTestingHttpClient/MyCoolService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UnitTestingHttpClient
+{
+    public class MyCoolService
+    {
+
+        private readonly HttpMessageHandler _messageHandler;
+
+        public MyCoolService(HttpMessageHandler messageHandler)
+        {
+            _messageHandler = messageHandler;
+        }
+
+        public MyCoolService() : this(new HttpClientHandler())
+        {
+        }
+
+        public async Task<string> GetGoogleContent()
+        {
+            using (var httpClient = new HttpClient(_messageHandler))
+            {
+                var response = await httpClient.GetAsync("http://www.google.com/");
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
---
+++ UnitTestingHttpClient/MyCoolServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+
+namespace UnitTestingHttpClient
+{
+    [TestClass]
+    public class MyCoolServiceTests
+    {
+        [TestMethod]
+        public async Task ActuallyGoToGoogle()
+        {
+            var content = await new MyCoolService().GetGoogleContent();
+
+            Assert.IsNotNull(content);
+        }
+
+        [TestMethod]
+        public async Task MockGoToGoogle()
+        {
+            var handler = new Mock<HttpMessageHandler>();
+
+            const string expected = "Yo! Just call me Test Google!!!";
+
+            handler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .Returns(Task<HttpResponseMessage>.Factory.StartNew(() => 
+                    new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent(expected)
+                    }));
+
+            var actual = await new MyCoolService(handler.Object).GetGoogleContent();
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
